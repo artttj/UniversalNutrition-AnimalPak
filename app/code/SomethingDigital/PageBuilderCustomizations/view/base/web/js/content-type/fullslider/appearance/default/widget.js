81632ecc2c915a798b0c655fd04863c54d5cbcbf
Natalia Shabaieva <natalia.shdnepr@gmail.com>
2020-01-13 00:31:50 +0200

    Update "Full Slider" content type to have a mobile-only option

--- app/code/SomethingDigital/PageBuilderCustomizations/view/base/web/js/content-type/fullslider/appearance/default/widget.js
+++ app/code/SomethingDigital/PageBuilderCustomizations/view/base/web/js/content-type/fullslider/appearance/default/widget.js
@@ -1,8 +1,10 @@
 define([
     'jquery',
     'Magento_PageBuilder/js/events',
-    'slick'
-], function ($, events) {
+    'slick',
+    'SomethingDigital_PageBuilderCustomizations/js/breakpoints',
+    'matchMedia'
+], function ($, events, slick, breakpoints, mediaCheck) {
     'use strict';
 
     return function (config, sliderElement) {
@@ -15,14 +17,32 @@ define([
             $element.slick('unslick');
         }
 
-        $element.slick({
+        var settings = {
             autoplay: $element.data('autoplay'),
             autoplaySpeed: $element.data('autoplay-speed') || 0,
             fade: $element.data('fade'),
             infinite: $element.data('is-infinite'),
             arrows: $element.data('show-arrows'),
             dots: $element.data('show-dots')
-        });
+        };
+        $element.slick(settings);
+
+        if ($element.data('mobile-only')) {
+            mediaCheck({
+                media: '(min-width: ' + (breakpoints.screen__m + 1) + 'px)',
+                entry: function () {
+                    if ($element.hasClass('slick-initialized')) {
+                        $element.slick('unslick');
+                        $element.show();
+                    }
+                },
+                exit: function () {
+                    if (!$element.hasClass('slick-initialized')) {
+                        return $element.slick(settings);
+                    }
+                }
+            });
+        }
 
         // Redraw slide after content type gets redrawn
         events.on('contentType:redrawAfter', function (args) {
