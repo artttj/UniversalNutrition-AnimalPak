663fffcbe284ff0970c45cbbf1a7265dba2885ea
Jayson DeMarchi <jayson.demarchi@gmail.com>
2020-04-26 13:39:52 -0400

    Add filename validation

--- cicd/push-db.sh
+++ cicd/push-db.sh
@@ -13,6 +13,12 @@ fi
 
 echo "Pushing $db_filename to Azure Storage"
 
+if [[ ! "${db_filename}" =~ ^20[0-9]{2}-[01][0-9]-[0-3][0-9]-[a-z\-]*-[a-z\-]*.sql.gz$ ]]; then
+    echo "Error: Filename format is not valid"
+    echo "  Expected format YYYY-MM-DD-IMAGE_NAME-BRANCH.sql.gz"
+    exit
+fi
+
 az storage blob upload \
     --container-name $IMAGE_NAME \
     --file $db_path \
