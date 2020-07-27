9e49173a06b103236cb600a4aeaf152faa7e30e8
Jayson DeMarchi <jayson.demarchi@gmail.com>
2020-07-27 01:26:23 -0400

    Add AnimalPak cicd vars

--- cicd/vars.sh
+++ cicd/vars.sh
@@ -10,14 +10,14 @@ fi
 . "${CICD_DIR}/logger.sh"
 
 export AZURE_STORAGE_ACCOUNT=${AZURE_STORAGE_ACCOUNT:-"sasdmagentodev"}
-export BASE_DOMAIN=${BASE_DOMAIN:-"magento.test"}
+export BASE_DOMAIN=${BASE_DOMAIN:-"animalpak.test"}
 export IMAGE_REGISTRY=${IMAGE_REGISTRY:-"sdmagentodev.azurecr.io"}
-export IMAGE_NAME=${IMAGE_NAME:-accelerator}
+export IMAGE_NAME=${IMAGE_NAME:-animalpak}
 export IMAGE_TAG=${IMAGE_TAG:-${BUILD_SOURCEBRANCHNAME:-develop}}
 export MAGE_BASE_VERSION=${MAGE_BASE_VERSION:-7.2}
 export MAGE_BASE_IMAGE=${MAGE_BASE_IMAGE:-${IMAGE_REGISTRY}/base-images/magento-php-fpm:${MAGE_BASE_VERSION}}
 
-export INIT_DB_PATH=${INIT_DB_PATH:-"accelerator/2020-04-22-accelerator-${DB_BRANCH:-develop}.sql.gz"}
+export INIT_DB_PATH=${INIT_DB_PATH:-"animalpak/2020-07-25-animalpak-${DB_BRANCH:-develop}.sql.gz"}
 export TEST_WAIT_SECS=${TEST_WAIT_SECS:-30}
 
 echo "Using Variables:"
