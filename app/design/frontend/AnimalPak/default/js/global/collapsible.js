101872e733ef81f3dcc128cb3285c7e17a40c189
Kristian Charbonneau <kcharbonneau@somethingdigital.com>
2020-07-28 10:12:04 -0400

    Create new theme with createTheme.sh

---
+++ app/design/frontend/AnimalPak/default/js/global/collapsible.js
@@ -0,0 +1,34 @@
+import $ from 'jquery';
+import peekaboo from 'peekaboo-toggle'
+import domready from 'domready';
+import breakpoints from 'utils/breakpoints';
+
+const collapsible = () => {
+  domready(() => {
+    const initPeekaboo = () => {
+      $('.block-collapsible-nav-title').peekaboo();
+    };
+
+    $(document).on(
+      'contentUpdated',
+      () => {
+        initPeekaboo();
+      }
+    );
+    initPeekaboo();
+    window.require(['matchMedia'], mediaCheck => {
+      mediaCheck({
+        // that's (min-width: ($screen__m + 1px)), as defined in _variables.scss
+        media: `(min-width: ${breakpoints.screen__m + 1}px)`,
+        entry() {
+          $('.block-collapsible-nav-title[aria-expanded="false"]').trigger('click');
+        },
+        exit() {
+          $('.block-collapsible-nav-title[aria-expanded="true"]').trigger('click');
+        },
+      });
+    });
+  });
+};
+
+export default collapsible;
