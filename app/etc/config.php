bcd8f3c8c6fd85b3cd8231962a4bd312a3f3eb3b
Kristian Charbonneau <kcharbonneau@somethingdigital.com>
2020-08-05 17:17:20 -0400

    Enable Unirgy modules

--- app/etc/config.php
+++ app/etc/config.php
@@ -364,13 +364,17 @@ return [
         'SomethingDigital_SidebarMinicart' => 1,
         'SomethingDigital_VirtualThemes' => 1,
         'Temando_ShippingRemover' => 1,
+        'Unirgy_RapidFlow' => 1,
+        'Unirgy_RapidFlowPro' => 1,
+        'Unirgy_SimpleUp' => 1,
+        'Unirgy_SimpleLicense' => 1,
         'Vertex_Tax' => 1,
         'Vertex_AddressValidation' => 1,
-        'Yotpo_Loyalty' => 1,
         'Xtento_XtCore' => 1,
         'Xtento_StockImport' => 1,
         'Xtento_TrackingImport' => 1,
         'Xtento_OrderExport' => 1,
+        'Yotpo_Loyalty' => 1,
         'Yotpo_Yotpo' => 1
     ],
     'scopes' => [
